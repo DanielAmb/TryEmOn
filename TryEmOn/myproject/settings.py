a5aee625ad4d9cbe2a09da81a251d92613742db0
DanielAmb <dba99blue@gmail.com>
2024-09-30 09:33:11 -0400

    added static files (images, js, css)

--- TryEmOn/myproject/settings.py
+++ TryEmOn/myproject/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "myapp/static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
